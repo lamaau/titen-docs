4b9f08e53d6fdad7ffc313d95d32699574bde2b3
rizkhal <lamaaurizkhal@gmail.com>
2022-05-09 02:30:52 +0900

    baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://codegoen.lamaau.space',
-  baseUrl: '/',
+  baseUrl: '/codegoen/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
